c787641497f8cdcf3c741f1edf7efabf8abc7cad
cyph3r.exe <67096012+cyph3r-exe@users.noreply.github.com>
2022-07-18 23:42:46 +0530

    remove ios support + sidebar changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const appTitle = 'Tech For Good';
-    Colors.amberAccent;
+    const appTitle = '#TechForGood';
+    const TextStyle(
+      fontStyle: FontStyle.italic,
+      fontSize: 24,
+    );
 
     return MaterialApp(
       title: appTitle,
@@ -43,45 +46,73 @@ class NavigationDrawer extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               buildHeader(context),
-              buildMenuItems(context,)
+              buildMenuItems(
+                context,
+              )
             ],
           ),
         ),
       );
 }
-Widget buildHeader(BuildContext context) => Container(
-  color: const Color.fromARGB(255, 139, 184, 71),
-  padding: EdgeInsets.only(
-    top: MediaQuery.of(context).padding.top,
-  ),
-  child: Column(
-    children: const [
-      CircleAvatar(
-        radius: 52,
-        backgroundImage: NetworkImage(
-          'https://media.discordapp.net/attachments/758708402938576897/998587392468463697/zenith_logo_without_bg.png'
-        ),
-      ),
-      SizedBox(height: 12,),
-      Text(
-        'Futurz Club',
-        style: TextStyle(fontSize: 28, color: Colors.white70),
-      ),
-      Text(
-        'futurz.afgji@gmail.com',
-        style: TextStyle(fontSize: 16, color: Colors.white24),
-      )
-    ],
-  ),
-);
 
-Widget buildMenuItems(BuildContext context) => Column(
-      children: [
-        ListTile(
-          title: const Text(''),
-          onTap: () {},
-        ),
-      ],
+Widget buildHeader(BuildContext context) => Container(
+      color: const Color.fromARGB(224, 92, 206, 140),
+      padding: EdgeInsets.only(
+        top: MediaQuery.of(context).padding.top,
+      ),
+      child: Column(
+        children: const [
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: NetworkImage(
+                'https://media.discordapp.net/attachments/758708402938576897/998587392468463697/zenith_logo_without_bg.png'),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Text(
+            'Futurz Club',
+            style: TextStyle(
+                fontSize: 28,
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontWeight: FontWeight.bold),
+          ),
+          Text(
+            'futurz.afgji@gmail.com',
+            style: TextStyle(fontSize: 16, color: Colors.white70),
+          )
+        ],
+      ),
+    );
+
+Widget buildMenuItems(BuildContext context) => Container(
+      color: Colors.white,
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Text(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontFamily: 'Arial',
+                fontStyle: FontStyle.italic,
+                fontSize: 20,
+                color: Colors.black.withOpacity(0.6)),
+          ),
+          Container(
+            child: Column(
+              children: const [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 150,
+                  backgroundImage: NetworkImage(
+                      'https://media.discordapp.net/attachments/758708402938576897/998628585453666404/Futurz_Initiative_Without_BG.png?width=499&height=499'),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
 
 class MyCustomForm extends StatelessWidget {
@@ -89,8 +120,28 @@ class MyCustomForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
+    return Container(
+      decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              stops: [
+                0.1,
+                0.4,
+                0.6,
+                0.9,
+              ],
+              colors: [
+                Color.fromARGB(255, 89, 255, 213),
+                Color.fromARGB(255, 113, 255, 137),
+                Color.fromARGB(255, 89, 255, 103),
+                Colors.teal,
+              ],
+            )
+          ),
+      child: Column(
+        children: [
+        
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
           child: TextFormField(
@@ -121,6 +172,7 @@ class MyCustomForm extends StatelessWidget {
             controller: myController3,
           ),
         ),
+        
         ElevatedButton(
           onPressed: () async {
             final result = await FilePicker.platform.pickFiles();
@@ -137,7 +189,44 @@ class MyCustomForm extends StatelessWidget {
           },
           child: const Text(insideText),
         ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+          child: Text('“Progress is impossible without change, and those who cannot change their minds cannot change anything.”',
+          textAlign: TextAlign.start,
+        style: TextStyle(
+          fontSize: 30,
+          fontFamily: 'Roboto',
+          fontStyle: FontStyle.italic,
+          color: Color.fromARGB(153, 46, 44, 44)
+          ),
+        ),
+      ),
+      const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 0, vertical: 2),
+          child: Text('Developed by',
+          textAlign: TextAlign.start,
+        style: TextStyle(
+          fontSize: 10,
+          fontFamily: 'Roboto',
+          fontStyle: FontStyle.italic,
+          color: Color.fromARGB(153, 46, 44, 44)
+          ),
+        ),
+      ),
+      const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 2, vertical: 4),
+          child: Text('Futurz Club AFGJI',
+          textAlign: TextAlign.start,
+          style: TextStyle(
+          fontSize: 12,
+          fontFamily: 'Roboto',
+          fontWeight: FontWeight.bold,
+          color: Color.fromARGB(153, 46, 44, 44)
+          ),
+        ),
+      ),
       ],
+      ),
     );
   }
 }
